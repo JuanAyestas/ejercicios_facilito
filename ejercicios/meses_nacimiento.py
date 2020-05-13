23c9f0174704ee3408a05d527f80cd9a1f414d66
Astor Ayestas <astor_david7@hotmail.com>
2020-05-13 01:43:08 -0600

    fixed the algorythm

--- ejercicios/meses_nacimiento.py
+++ ejercicios/meses_nacimiento.py
@@ -5,6 +5,30 @@ from datetime import date, datetime, timedelta
 #month = week * 4.34
 #year = month * 12
 
+
+def full_date(number):
+  años = (abs(now.year - año - number))
+  meses = (abs(now.month - mes))
+  dias = (abs(now.day - dia))
+  print("Han transcurrido {} años, {} meses, y {} días desde su nacimiento".format(
+      años, meses, dias))
+  detalle = input("""  Si quiere saber su edad completa en meses, semanas o días...
+    Ingrese 'meses', 'semanas' o 'dias'\n""")
+  while (detalle != "Terminar" and detalle != "terminar"):
+    if detalle == "meses" or detalle == "Meses":
+      mes_total = (años * 12 + (now.month - mes))
+      print("En total, su edad es de {} meses".format(mes_total))
+    elif detalle == "semanas" or detalle == "Semanas":
+      semanas_total = (round((años * 12 + (now.month - mes)) * 52, 2))
+      print("En total, su edad es de {} semanas".format(semanas_total))
+    elif detalle == "dias" or detalle == "Dias":
+      dias_total = (round(((años * 12 + (now.month - mes)) * 52) * 7.024, 2))
+      print("En total, su edad es de {} días".format(dias_total))
+    else:
+      print("Revise si ingresó correctamente la opción e inténtelo de nuevo.")
+    detalle = input("  Ingrese 'meses', 'semanas', 'dias' o 'terminar' para finalizar\n")
+
+
 def current_date_format(date):
     months = ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
               "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
@@ -23,32 +47,19 @@ año = int(input("Introduzca su año de nacimiento: "))
 
 if año >= 1900:
   print("""  Enero (1), Febrero (2), Marzo (3), Abril (4), Mayo (5), Junio (6), 
-  Julio (7), Agosto (8), Septiembre (9), Octubre (10), Noviembre (11), Diciembre (12)""")
+    Julio (7), Agosto (8), Septiembre (9), Octubre (10), Noviembre (11), Diciembre (12)""")
   mes = int(input("Introduzca el número del mes de nacimiento: "))
   if mes <= 12 and mes >=1:
     dia = int(input("Introduzca su día de nacimiento: "))
     if dia <= 31 and dia >=1:
       date = datetime(año, mes, dia)
       print("Nació el {}".format(current_date_format(date)))
-      años = (abs(now.year - año - 1))
-      meses = (abs(now.month - mes))
-      dias = (abs(now.day - dia))
-      print("Han transcurrido {} años, {} meses, y {} días desde su nacimiento".format(años, meses, dias))
-      detalle = input("""  Si quiere saber su edad completa en meses, semanas o días...
-  Ingrese 'meses', 'semanas' o 'dias'\n""")
-      while (detalle != "Terminar" and detalle != "terminar" ):
-        if detalle == "meses" or detalle == "Meses":  
-          mes_total = (años * 12 + (now.month - mes))
-          print("En total, su edad es de {} meses".format(mes_total))
-        elif detalle == "semanas" or detalle == "Semanas":
-          semanas_total = (round((años * 12 + (now.month - mes)) * 52, 2))
-          print("En total, su edad es de {} semanas".format(semanas_total))
-        elif detalle == "dias" or detalle == "Dias":
-          dias_total = (round(((años * 12 + (now.month - mes)) * 52) * 7.024, 2))
-          print("En total, su edad es de {} días".format(dias_total))
-        else:
-          print("Revise si ingresó correctamente la opción e inténtelo de nuevo.")
-        detalle = input("  Ingrese 'meses', 'semanas', 'dias' o 'terminar' para finalizar\n")
+      if mes >= now.month:
+        if dia >= now.day or dia <= now.day:
+          full_date(1)
+      elif mes <= now.month:
+        if dia <= now.day or dia >= now.day:
+          full_date(0)
     else:
       print("Revise si ingresó correctamente los números e inténtelo de nuevo.")
   else:
--- ejercicios/months_born.py
+++ ejercicios/months_born.py
@@ -4,6 +4,27 @@ from datetime import date, datetime, timedelta
 #month = week * 4.34
 #year = month * 12
 
+def full_date(number):
+  años = (abs(now.year - año - number))
+  meses = (abs(now.month - mes))
+  dias = (abs(now.day - dia))
+  print("It's been {} years, {} months, y {} days since you were born.".format(
+      años, meses, dias))
+  detalle = input("""  If you want to know how old are you in months, weeks or days...
+    Enter 'months', 'weeks' or 'days'\n""")
+  while (detalle != "end" and detalle != "End"):
+    if detalle == "months" or detalle == "Months":
+      mes_total = (años * 12 + (now.month - mes))
+      print("In total, your age is {} months old".format(mes_total))
+    elif detalle == "weeks" or detalle == "Weeks":
+      semanas_total = (round((años * 12 + (now.month - mes)) * 52, 2))
+      print("In total, your age is {} weeks old".format(semanas_total))
+    elif detalle == "days" or detalle == "Days":
+      dias_total = (round(((años * 12 + (now.month - mes)) * 52) * 7.024, 2))
+      print("In total, your age is {} days old".format(dias_total))
+    else:
+      print("Check if you entered the right input, then try again.")
+    detalle = input("  Enter 'months', 'weeks', 'days' or 'end' to finish.\n")
 
 def current_date_format(date):
     months = ("January", "February", "March", "April", "May", "June", "July",
@@ -30,27 +51,12 @@ if año >= 1900:
     if dia <= 31 and dia >= 1:
       date = datetime(año, mes, dia)
       print("You were born in {}".format(current_date_format(date)))
-      años = (abs(now.year - año - 1))
-      meses = (abs(now.month - mes))
-      dias = (abs(now.day - dia))
-      print("It's been {} years, {} months, y {} days since you were born.".format(
-          años, meses, dias))
-      detalle = input("""  If you want to know how old are you in months, weeks or days...
-  Enter 'months', 'weeks' or 'days'\n""")
-      while (detalle != "end" and detalle != "End"):
-        if detalle == "months" or detalle == "Months":
-          mes_total = (años * 12 + (now.month - mes))
-          print("In total, your age is {} months old".format(mes_total))
-        elif detalle == "weeks" or detalle == "Weeks":
-          semanas_total = (round((años * 12 + (now.month - mes)) * 52, 2))
-          print("In total, your age is {} weeks old".format(semanas_total))
-        elif detalle == "days" or detalle == "Days":
-          dias_total = (
-              round(((años * 12 + (now.month - mes)) * 52) * 7.024, 2))
-          print("In total, your age is {} days old".format(dias_total))
-        else:
-          print("Check if you entered the right input, then try again.")
-        detalle = input("  Enter 'months', 'weeks', 'days' or 'end' to finish.\n")
+      if mes >= now.month:
+        if dia >= now.day or dia <= now.day:
+          full_date(1)
+      elif mes <= now.month:
+        if dia <= now.day or dia >= now.day: 
+          full_date(0)
     else:
       print("Check if you entered a valid number, then try again.")
   else:
